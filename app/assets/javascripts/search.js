b6052db0180de7be2d94cd52bfc597a6ad065de3
David Wen <dstwen@gmail.com>
2014-01-05 17:48:01 -0800

    move js flight ajax call into a function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $("body").on("click", "#search-button", function() {
-    var url = $(this).attr("data-flight-results-url");
+    callFlyShortcut($(this))
+  });
+
+  var callFlyShortcut = function(this)  {
+    var url = this.attr("data-flight-results-url");
     $.ajax({
       url: url
     })
     .done(function(data) {
       $(".results").after(data.results);
     });
-  });
+  }
 });
